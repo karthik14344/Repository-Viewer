13f55aac51de538b644ab3a07fe1543a6533bd3c
karthik14344 <singidikarthik1434@gmail.com>
2023-07-06 23:48:28 +0530

    sixth commit

--- lib/auth/presentation/sign_in_page.dart
+++ lib/auth/presentation/sign_in_page.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'package:path/path.dart';
 
 @RoutePage()
 class SignInPage extends StatelessWidget {
