2b402c730062560300a96bd959f243af36b8e966
karthik14344 <singidikarthik1434@gmail.com>
2023-07-07 13:35:04 +0530

    eighth commit

--- lib/starred_repos/presentation/starred_repos_page.dart
+++ lib/starred_repos/presentation/starred_repos_page.dart
@@ -1,12 +1,23 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:repo_viewer/auth/shared/providers.dart';
 
 @RoutePage()
-class StarredReposPage extends StatelessWidget {
+class StarredReposPage extends ConsumerWidget {
   const StarredReposPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold();
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            ref.read(authNotifierProvider.notifier).signOut();
+          },
+          child: const Text('Sign Out'),
+        ),
+      ),
+    );
   }
 }
