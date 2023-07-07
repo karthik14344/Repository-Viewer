275505c6c4059d9743e1a8a8eea8e0f7f9b25a56
karthik14344 <singidikarthik1434@gmail.com>
2023-07-08 02:12:18 +0530

    Fourteenth Commit

--- lib/github/core/infrastructure/user_dto.dart
+++ lib/github/core/infrastructure/user_dto.dart
@@ -3,6 +3,8 @@
 // git push
 
 import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../domain/user.dart';
 part 'user_dto.freezed.dart';
 part 'user_dto.g.dart';
 
@@ -11,10 +13,28 @@ class UserDTO with _$UserDTO {
   const UserDTO._();
   const factory UserDTO({
     @JsonKey(name: 'login') required String name,
+    //this is to change the name in produced 'user_dto.g.dart' file as per the
+    //GET request produced in github.rest
     @JsonKey(name: 'avatar_url') required String avatarUrl,
+    //this is to change the name in produced 'user_dto.g.dart' file as per the
+    //GET request produced in github.rest
   }) = _UserDTO; //API error
 
-  //this is to convert the json resposes into code variables
+  //this is to convert the json responses into code variables
   factory UserDTO.fromJson(Map<String, dynamic> json) =>
       _$UserDTOFromJson(json);
+
+  factory UserDTO.fromDomain(User _) {
+    return UserDTO(
+      name: _.name,
+      avatarUrl: _.avatarUrl,
+    );
+  }
+
+  User toDomain() {
+    return User(
+      name: name,
+      avatarUrl: avatarUrl,
+    );
+  }
 }
